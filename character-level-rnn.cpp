23a06002ac456216fe5833f514ca465e0030420e
Jack Gallagher <33359426+OdysseusGit@users.noreply.github.com>
2018-03-26 15:18:40 +0100

    Update character-level-rnn.cpp

--- character-level-rnn.cpp
+++ character-level-rnn.cpp
@@ -1,7 +1,7 @@
 /*
- *	Vanilla recurrent neural network that performs basic character generation.
- *	A C++ implementation of the example given in Andrej Karpathy's "The Unreasonable Effectiveness of Recurrent Neural Networks".
- *	Reference: http://karpathy.github.io/2015/05/21/rnn-effectiveness/
+ * Vanilla recurrent neural network that performs basic character generation.
+ * A C++ implementation of the example given in Andrej Karpathy's "The Unreasonable Effectiveness of Recurrent Neural Networks".
+ * Reference: http://karpathy.github.io/2015/05/21/rnn-effectiveness/
 */
 
 #include <iostream>
